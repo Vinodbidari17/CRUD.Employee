f3c2af4f4c2e2fc1d817d7ad295b690673f35aab
Vinodbidari17 <vinodeb123@gmail.com>
2025-08-08 23:47:06 +0530

    This is first commit

---
+++ crud.employee/src/main/java/crud/employee/controller/EmployeeController.java
@@ -0,0 +1,65 @@
+package crud.employee.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import crud.employee.entity.Employee;
+import crud.employee.service.EmployeeService;
+
+@Controller
+public class EmployeeController {
+	@Autowired
+	EmployeeService service;
+	
+	@PostMapping("/create")
+	public void createNewEmployee(@ModelAttribute Employee emp) {
+		service.createNewEmployee(emp);
+	}
+	@PostMapping("/update")
+	public void updateEmployee(@ModelAttribute Employee emp) {
+		service.updateEmployee(emp);
+	}
+	@GetMapping("/delete")
+	public void deleteEmployee(Long id) {
+		service.deleteEmployee(id);
+	}
+	@GetMapping("/search")
+	public String searchEmployee(Long id, Model model) {
+		Employee emp = service.searchEmployee(id);
+		// System.out.println(emp);
+		model.addAttribute("empObj", emp);
+		return "searchResult";
+	}
+	
+	@GetMapping("/viewAll")
+	public String getAllEmployees(Model model) {
+		List<Employee> empList = service.getAllEmployees();
+		System.out.println(empList);
+		model.addAttribute("empList", empList);
+		return "viewAll";
+	}
+	
+	
+	@GetMapping("/createPage")
+	public String createPage() {
+		return "create";   // create.html
+	}
+	@GetMapping("/searchPage")
+	public String searchPage() {
+		return "search";   // search.html
+	}
+	@GetMapping("/updatePage")
+	public String updatePage() {
+		return "update";   // update.html
+	}
+	@GetMapping("/deletePage")
+	public String deletePage() {
+		return "delete";   // delete.html
+	}
+}
---
+++ crud.employee/src/main/java/crud/employee/service/EmployeeServiceImplementation.java
@@ -0,0 +1,36 @@
+package crud.employee.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import crud.employee.entity.Employee;
+import crud.employee.repository.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImplementation implements EmployeeService {
+	
+	@Autowired
+	EmployeeRepository repo;
+	
+	public void createNewEmployee(Employee emp) {
+		repo.save(emp);
+	}
+	
+	public Employee searchEmployee(Long id) {
+		return repo.findById(id).get();
+	}
+	
+	public void updateEmployee(Employee emp) {
+		repo.save(emp);
+	}
+	
+	public void deleteEmployee(Long id) {
+		repo.deleteById(id);
+	}
+
+	public List<Employee> getAllEmployees() {
+		return repo.findAll();
+	}
+}
